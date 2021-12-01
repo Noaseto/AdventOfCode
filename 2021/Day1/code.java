f4873c57e3807e497f6a794fc10351974557433b
Noaseto <Noaseto@protonmail.com>
2021-12-01 21:59:20 +0100

    1 etoiles d'OR

--- 2021/Day1/code.java
+++ 2021/Day1/code.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 class code{
     public static void main (String[] args){
+        // part1();
+        part2();
+    }
+
+    public static void part1(){
+
         try{
             Scanner sc = new Scanner(new File("./2021/Day1/input"));
             String previousLine = sc.nextLine();
@@ -15,6 +21,38 @@ class code{
                 }
 
                 previousLine = currentLine;
+            }
+
+            System.out.println(numOfIncrease);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public static void part2(){
+        try{
+            Scanner sc = new Scanner(new File("./2021/Day1/input"));
+            
+            String firstLine = sc.nextLine();
+            String secondLine = sc.nextLine();
+            String thirdLine = sc.nextLine();
+
+            int numOfIncrease = 0;
+            int triplet = Integer.parseInt(firstLine)+Integer.parseInt(secondLine)+Integer.parseInt(thirdLine);
+
+            while(sc.hasNextLine()){
+                firstLine = secondLine;
+                secondLine = thirdLine;
+                thirdLine = sc.nextLine();
+
+                int curTriplet = Integer.parseInt(firstLine)+Integer.parseInt(secondLine)+Integer.parseInt(thirdLine);
+
+                if((curTriplet - triplet)>0){
+                    numOfIncrease++;
+                }
+
+                triplet = curTriplet;
             }
 
             System.out.println(numOfIncrease);
